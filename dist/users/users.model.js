dcb3ca6e4ebc75534f3f7a8865638666d62a109f
Genaro Figueiredo <genarofigueiredo@hotmail.com>
2020-04-20 11:34:40 -0300

    adicionado a busca por um id do usuario.

--- dist/users/users.model.js
+++ dist/users/users.model.js
@@ -1,13 +1,23 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const users = [
-    { name: "Genaro Figueiredo", email: "genaro@gmail.com" },
-    { name: "Geomar Figueiredo", email: "geomar@gmail.com" },
-    { name: "Jorge Figueiredo", email: "jorge@gmail.com" },
+    { id: "1", name: "Genaro Figueiredo", email: "genaro@gmail.com" },
+    { id: "2", name: "Geomar Figueiredo", email: "geomar@gmail.com" },
+    { id: "3", name: "Jorge Figueiredo", email: "jorge@gmail.com" },
 ];
 class User {
     static findAll() {
         return Promise.resolve(users);
+    }
+    static findById(id) {
+        return new Promise((resolve) => {
+            const filtered = users.filter((user) => user.id === id);
+            let user = undefined;
+            if (filtered.length > 0) {
+                user = filtered[0];
+            }
+            resolve(user);
+        });
     }
 }
 exports.User = User;
--- users/users.model.ts
+++ users/users.model.ts
@@ -1,11 +1,22 @@
 const users = [
-  { name: "Genaro Figueiredo", email: "genaro@gmail.com" },
-  { name: "Geomar Figueiredo", email: "geomar@gmail.com" },
-  { name: "Jorge Figueiredo", email: "jorge@gmail.com" },
+  { id: "1", name: "Genaro Figueiredo", email: "genaro@gmail.com" },
+  { id: "2", name: "Geomar Figueiredo", email: "geomar@gmail.com" },
+  { id: "3", name: "Jorge Figueiredo", email: "jorge@gmail.com" },
 ];
 
 export class User {
   static findAll(): Promise<any[]> {
     return Promise.resolve(users);
+  }
+
+  static findById(id: string): Promise<any> {
+    return new Promise((resolve) => {
+      const filtered = users.filter((user) => user.id === id);
+      let user = undefined;
+      if (filtered.length > 0) {
+        user = filtered[0];
+      }
+      resolve(user);
+    });
   }
 }
